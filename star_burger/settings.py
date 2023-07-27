d28f2ea240e00cfffed9c0bcf34aa7bcbbbcb4a3
Jaggmort <djeedo@gmail.com>
2023-07-27 18:55:44 +0300

    Fix bash

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,7 +15,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', 'localhost')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
