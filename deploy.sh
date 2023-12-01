de39331710ffcb55c3c9d9310e62a55609efdf1d
Jaggmort <djeedo@gmail.com>
2023-12-01 10:31:56 +0300

    Fix docker compose

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ cd /opt/star-burger/
 docker compose down
 systemctl stop nginx
 docker compose up -d
-docker compose exec django python manage.py migrate --noinput
+docker compose exec django python manage.py migrate
 docker compose exec django python manage.py collectstatic --no-input 
 printf "\nDeploy completed!\n"
--- first_deploy.sh
+++ first_deploy.sh
@@ -5,6 +5,6 @@ systemctl stop nginx
 mkdir -p nginx/letsencrypt/
 cp -a /etc/letsencrypt/ /opt/star-burger/nginx/
 docker compose up -d
-sudo docker exec -i star-burger-django-1 python manage.py migrate --noiput
+sudo docker exec -i star-burger-django-1 python manage.py migrate 
 sudo docker exec -i star-burger-django-1 python manage.py loaddata data.json
 printf "\nDeploy completed!\n"
