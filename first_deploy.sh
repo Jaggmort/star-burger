727c6675805b59056a45f16a5415d7d728c236ca
Jaggmort <djeedo@gmail.com>
2023-12-01 10:14:24 +0300

    Docer compose fix

--- first_deploy.sh
+++ first_deploy.sh
@@ -4,7 +4,6 @@ cd /opt/star-burger/
 systemctl stop nginx
 mkdir -p nginx/letsencrypt/
 cp -a /etc/letsencrypt/ /opt/star-burger/nginx/
-chmod u+x entrypoint.prod.sh
 docker compose up -d --build
 sudo docker exec -i star-burger-django-1 python manage.py migrate --noiput
 sudo docker exec -i star-burger-django-1 python manage.py loaddata data.json
